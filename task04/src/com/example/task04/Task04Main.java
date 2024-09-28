a63758fff6e147d523b5df6f77e1f4c5f30ab15f
Alexander <itskilobyte@gmail.com>
2024-09-28 15:16:02 +0500

    Completed 4th task.

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,12 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Point first = new Point(10, 5);
+        Point second = new Point(15, 10);
+        Line line = new Line(first, second);
 
+        System.out.println(line);
+        System.out.println(line.isCollinearLine(new Point(0, 0)));
+        System.out.println(line.isCollinearLine(new Point(12, 7)));
     }
 }
