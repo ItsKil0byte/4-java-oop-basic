aaf5003d0a520e9680d27449cff362f018725a88
Alexander <itskilobyte@gmail.com>
2024-09-28 16:06:21 +0500

    Completed 5th task.

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,9 +1,16 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+    private final ArrayList<Point> points;
+
+    public PolygonalLine() {
+        points = new ArrayList<>();
+    }
 
     /**
      * Устанавливает точки ломаной линии
@@ -11,7 +18,11 @@ public class PolygonalLine {
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        this.points.clear();
+
+        for (Point point : points) {
+            addPoint(point);
+        }
     }
 
     /**
@@ -20,7 +31,7 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        points.add(new Point(point.getX(), point.getY()));
     }
 
     /**
@@ -30,7 +41,7 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        points.add(new Point(x, y));
     }
 
     /**
@@ -39,8 +50,11 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0.0;
+        for (int i = 1; i < points.size(); i++) {
+            length += points.get(i - 1).getLength(points.get(i));
+        }
+        return length;
     }
 
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,9 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
-
+        PolygonalLine line = new PolygonalLine();
+        line.addPoint(10, 10);
+        line.addPoint(12, 12);
+        System.out.println(line.getLength());
     }
 }
